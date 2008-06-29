7003b8946966d395d75c41f4c7c3dc45c96ccc9b
jan.javorek <jan.javorek@57946991-b244-0410-a4a7-6722157904d4>
2008-06-29 16:45:28 +0000

    JDriver sample.
    git-svn-id: http://joss-cms.googlecode.com/svn/trunk@83 57946991-b244-0410-a4a7-6722157904d4

---
+++ class/driver/JDSample.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Joss framework & content management system.
+ *
+ * Created 29.6.2008.
+ *
+ * @author    Jan (Honza) Javorek aka Littlemaple http://www.javorek.net
+ * @copyright Copyright (c) 2008 Jan Javorek
+ * @package   Joss
+ * @link      http://code.google.com/p/joss-cms/
+ * @license   GNU GENERAL PUBLIC LICENSE version 2
+ */
+
+
+
+/**
+ * Driver sample.
+ *
+ * @author     Jan (Honza) Javorek aka Littlemaple <honza@javorek.net>
+ * @copyright  Copyright (c) 2008 Jan Javorek
+ * @package    Joss
+ * @version    $Revision$ ($Date$, $Author$)
+ */
+class JDSample extends JDriver {
+	
+	public function __construct($id) {
+		echo 'driver';
+	}
+	
+}
